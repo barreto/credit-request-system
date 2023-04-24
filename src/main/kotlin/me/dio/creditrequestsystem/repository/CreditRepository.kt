93d6f356d262dfd574eb72d3b89e637b5d975e2b
Barreto <v.barreto.and@gmail.com>
2023-04-24 08:28:58 -0300

    add data persistence layer with repositories

---
+++ src/main/kotlin/me/dio/creditrequestsystem/repository/CreditRepository.kt
@@ -0,0 +1,8 @@
+package me.dio.creditrequestsystem.repository
+
+import me.dio.creditrequestsystem.entity.Credit
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface CreditRepository : JpaRepository<Credit, Long>
