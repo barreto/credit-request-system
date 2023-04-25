2563565a0bc8e0b498ff8ef64daec37eac9d6567
Barreto <v.barreto.and@gmail.com>
2023-04-25 08:47:16 -0300

    add interfaces to allow apply solid principals

---
+++ src/main/kotlin/me/dio/creditrequestsystem/service/ICreditService.kt
@@ -0,0 +1,14 @@
+package me.dio.creditrequestsystem.service
+
+import me.dio.creditrequestsystem.entity.Credit
+import java.util.UUID
+
+interface ICreditService {
+
+    fun save(credit: Credit): Credit
+
+    fun findAllByCustomer(customerId: Long): List<Credit>
+
+    fun findByCreditCode(customerId: Long, creditCode: UUID): Credit
+
+}
---
+++ src/main/kotlin/me/dio/creditrequestsystem/service/ICustomerService.kt
@@ -0,0 +1,13 @@
+package me.dio.creditrequestsystem.service
+
+import me.dio.creditrequestsystem.entity.Customer
+
+interface ICustomerService {
+
+    fun save(customer: Customer): Customer
+
+    fun findById(id: Long): Customer
+
+    fun delete(id: Long)
+
+}
