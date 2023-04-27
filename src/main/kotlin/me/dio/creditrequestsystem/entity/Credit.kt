04fcf2601558c5e3b16981b07629204bbe686e54
Barreto <v.barreto.and@gmail.com>
2023-04-26 22:55:58 -0300

    fix credit attribute name for number of installments

--- src/main/kotlin/me/dio/creditrequestsystem/entity/Credit.kt
+++ src/main/kotlin/me/dio/creditrequestsystem/entity/Credit.kt
@@ -23,7 +23,7 @@ data class Credit(
         val dayFirstInstallment: LocalDate,
 
         @Column(nullable = false)
-        val numberOfInstallment: Int = 0,
+        val numberOfInstallments: Int = 0,
 
         @Enumerated
         val status: Status = Status.InProgress,
