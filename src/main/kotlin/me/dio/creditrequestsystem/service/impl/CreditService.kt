7fa141a3dee8956738e8fc94ee4690c93321b6cf
Barreto <v.barreto.and@gmail.com>
2023-04-30 20:34:40 -0300

    add challenge solution of three months in future day first installment

--- src/main/kotlin/me/dio/creditrequestsystem/service/impl/CreditService.kt
+++ src/main/kotlin/me/dio/creditrequestsystem/service/impl/CreditService.kt
@@ -5,6 +5,7 @@ import me.dio.creditrequestsystem.exception.BusinessException
 import me.dio.creditrequestsystem.repository.CreditRepository
 import me.dio.creditrequestsystem.service.ICreditService
 import org.springframework.stereotype.Service
+import java.time.LocalDate
 import java.util.*
 
 @Service
@@ -12,7 +13,17 @@ class CreditService(
         private val creditRepository: CreditRepository,
         private val customerService: CustomerService
 ) : ICreditService {
+
+    private fun validateMaxDayFirstInstallment(date: LocalDate) {
+        val maxFutureDate = LocalDate.now().plusMonths(3)
+        val isNotValidDate = date.isAfter(maxFutureDate)
+
+        if (isNotValidDate) throw BusinessException("Max day of first installment is 3 months in the future from today.")
+    }
+
     override fun save(credit: Credit): Credit {
+        validateMaxDayFirstInstallment(credit.dayFirstInstallment)
+
         credit.apply {
             customer = customerService.findById(credit.customer?.id!!)
         }
